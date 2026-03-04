4ea2b6442d8cc5507cc524387037a7c023135094
keypoint21c <keypoint21c@gmail.com>
2026-03-04 16:45:27 +0900

    Implement fund recommendation based on industry
    Added a function to recommend funds based on industry and type.

--- analyzer.py
+++ analyzer.py
@@ -15,3 +15,27 @@ def parse_business_info(text):
     }
 
     return result
+def recommend_funds(info):
+
+    industry = info["item"]
+
+    funds = []
+
+    if "재생" in industry or "재활용" in industry:
+        funds.append({
+            "name":"환경부 자원순환 사업",
+            "site":"https://www.bizinfo.go.kr"
+        })
+
+    if "제조" in info["type"]:
+        funds.append({
+            "name":"중진공 정책자금",
+            "site":"https://www.kosmes.or.kr"
+        })
+
+    funds.append({
+        "name":"지자체 중소기업 육성자금",
+        "site":"https://www.bizinfo.go.kr"
+    })
+
+    return funds
